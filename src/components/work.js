091aa98fb039331d48e19bcaad99c5b88a8d8c8b
Ilias <mac@Ilias.local>
2020-07-29 13:08:58 +0100

    Add dynamic projects section

--- src/components/work.js
+++ src/components/work.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Link } from "gatsby";
+import { StaticQuery, graphql, Link } from "gatsby";
+import Img from "gatsby-image";
 export const Work = () => {
   return (
     <section className="p-12">
-      <div className=" w-full w-2/4 ">
+      <div className="w-2/4 ">
         <h5 className="text-3xl font-bold">What I&apos;ve been working on</h5>
       </div>
       <div className="grid  grid-cols-1 md:grid-cols-2 py-12">
@@ -14,42 +15,66 @@ export const Work = () => {
         </p>
       </div>
 
-      <div className="grid  grid-cols-1 md:grid-cols-2 py-12 col-gap-24 row-gap-8">
-        <div>
-          <img src={"/work-demo.jpg"} className="rounded" alt="Logo" />
-
-          <h5 className="text-xl  py-4 text-gray-600">HabitScript</h5>
-          <p className="text-2xl font-semibold">
-            Track your coding time from visual studio code, display coding
-            metrics on web app.
-          </p>
-          <button className="pt-4 pb-1 text-lg border-b-2">
-            <Link to="/">View Project Details</Link>
-          </button>
-        </div>
-
-        <div>
-          <img src={"/work-website.png"} className="rounded" alt="Logo" />
-          <h5 className="text-xl  py-4 text-gray-600">Makerlapse</h5>
-          <p className="text-2xl font-semibold">
-            Cross platform desktop app built with Electron JS that screenshots
-            of your work and make a timelapse video of it.
-          </p>
-          <button className="pt-4 pb-1 text-lg border-b-2">
-            <Link to="/">View Project Details</Link>
-          </button>
-        </div>
-
-        <div>
-          <img src={"/work-shopify.jpg"} className="rounded" alt="Logo" />
-          <h5 className="text-xl  py-4 text-gray-600">Shopify Store</h5>
-          <p className="text-2xl font-semibold">
-            eCommerce website built with Gatsby JS and Shopify
-          </p>
-          <button className="pt-4 pb-1 text-lg border-b-2">
-            <Link to="/">View Project Details</Link>
-          </button>
-        </div>
+      <div className="grid  grid-cols-1 md:grid-cols-2 py-12 col-gap-24 row-gap-12">
+        <StaticQuery
+          query={graphql`
+            {
+              allProjects: allMarkdownRemark(
+                filter: {
+                  frontmatter: { featured: { ne: true } }
+                  fileAbsolutePath: { regex: "/(projects)/.*\\\\.md$/" }
+                }
+                sort: { fields: frontmatter___date, order: ASC }
+              ) {
+                nodes {
+                  frontmatter {
+                    title
+                    slug
+                    description
+                    featuredImage {
+                      childImageSharp {
+                        fixed(
+                          height: 250
+                          width: 500
+                          quality: 100
+                          cropFocus: ATTENTION
+                        ) {
+                          ...GatsbyImageSharpFixed
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          `}
+          render={({ allProjects }) => (
+            <>
+              {allProjects.nodes.map((project, index) => (
+                <div key={index}>
+                  <Img
+                    imgStyle={{ objectFit: "cover" }}
+                    fixed={
+                      project.frontmatter.featuredImage.childImageSharp.fixed
+                    }
+                    className="rounded"
+                  />
+                  <h5 className="text-xl  py-4 text-gray-600">
+                    {project.frontmatter.title}
+                  </h5>
+                  <p className="text-2xl font-semibold">
+                    {project.frontmatter.description}
+                  </p>
+                  <button className="pt-4 pb-1 text-lg border-b-2">
+                    <Link to={project.frontmatter.slug}>
+                      View Project Details
+                    </Link>
+                  </button>
+                </div>
+              ))}
+            </>
+          )}
+        />
       </div>
     </section>
   );
