fde0eb6a036588960167d118206b25349074339c
Ilias <mac@Ilias.local>
2020-08-10 00:26:57 +0100

    Add MDX provider to make a custom style for mdx files

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,15 @@
 import "./src/css/style.css";
 import PropTypes from "prop-types";
-
+import { MDXProvider } from "@mdx-js/react";
+import { components } from "./src/layouts/post-layout";
 import React from "react";
 
 import { ThemeProvider } from "./src/context/ThemeContext";
 
 export const wrapRootElement = ({ element }) => (
-  <ThemeProvider>{element}</ThemeProvider>
+  <MDXProvider components={components}>
+    <ThemeProvider>{element}</ThemeProvider>
+  </MDXProvider>
 );
 
 wrapRootElement.propTypes = {
