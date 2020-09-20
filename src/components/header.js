38c2f1cbb13ad988e490eccf78f6335a93a39f39
iliashad <iliasshaddad18@gmail.com>
2020-09-20 22:32:33 +0100

    Update header design

--- src/components/header.js
+++ src/components/header.js
@@ -20,15 +20,6 @@ function Header() {
       <div className="flex flex-wrap  justify-between p-4 mx-auto md:p-8">
         <Link to="/">
           <h1 className="flex  no-underline">
-            <svg
-              className="w-8 h-8 mr-2 fill-current"
-              height="54"
-              viewBox="0 0 54 54"
-              width="54"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-            </svg>
             <span className="text-xl  tracking-tight">
               {site.siteMetadata.title}
             </span>
@@ -61,8 +52,8 @@ function Header() {
             },
 
             {
-              route: `/work`,
-              title: `Work`,
+              route: `/blog`,
+              title: `Blog`,
             },
             {
               route: `/contact`,
