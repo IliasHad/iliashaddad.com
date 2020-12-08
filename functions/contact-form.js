a421de47b9908c9264892787ae9f5d0ab1eded9c
iliashad <iliasshaddad18@gmail.com>
2020-12-08 08:30:43 +0100

    Update contact form function

--- functions/contact-form.js
+++ functions/contact-form.js
@@ -1,54 +1,40 @@
-var sendpulse = require("sendpulse-api");
+const nodemailer = require("nodemailer");
 
 const contactAddress = process.env.EMAIL;
+const mailer = nodemailer.createTransport({
+  service: "SendPulse", // no need to set host or port etc.
+  auth: {
+    user: process.env.EMAIL_ADDRESS,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
 
 exports.handler = function (event, context, callback) {
   const { data } = JSON.parse(event.body);
   console.log(data);
   const subject = `New ${data.projectType} Project with ${data.budget}`;
-
-  sendpulse.init(
-    process.env.API_USER_ID,
-    process.env.API_SECRET,
-    process.env.TOKEN_STORAGE
-  );
-
-  var email = {
-    html:
-      `  
-    <h2> ${data.projectType} - ${data.budget} </h2>
-    <p>${data.message}</p>
-  <br>
-  This email sent by  ${data.name}  - ${data.email}
-  ` || "[No message]",
-    text:
-      `  
- ${data.projectType} - ${data.budget} 
-  ${data.message}
-
-This email sent by  ${data.name}  - ${data.email}
-` || "[No message]",
-    subject: subject,
-    from: {
-      name: data.name,
-      email: data.email,
+  mailer.sendMail(
+    {
+      from: data.email,
+      to: [contactAddress],
+      subject: subject || "[No subject]",
+      html:
+        `  
+        <h2> ${data.projectType} - ${data.budget} </h2>
+        <p>${data.message}</p>
+      <br>
+      This email sent by  ${data.name}  - ${data.email}
+      ` || "[No message]",
     },
-    to: [
-      {
-        name: "Ilias Haddad",
-        email: contactAddress,
-      },
-    ],
-  };
-
-  sendpulse.smtpSendMail(function (error, info) {
-    if (error) {
-      callback(JSON.parse(error));
-    } else {
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({ success: true }),
-      });
+    function (error, info) {
+      if (error) {
+        callback(error);
+      } else {
+        callback(null, {
+          statusCode: 200,
+          body: JSON.stringify({ success: true }),
+        });
+      }
     }
-  }, email);
+  );
 };
