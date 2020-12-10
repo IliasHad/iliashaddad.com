3f429fb4d1faace3ed3d2006ff2b38c013144f10
iliashad <iliasshaddad18@gmail.com>
2020-12-10 16:14:44 +0100

    add OG image for home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,34 +7,50 @@ import { Articles } from "../components/articles";
 import { Testimonials } from "../components/testimonials";
 import { Work } from "../components/work";
 import { Skills } from "../components/skills";
-
+import { StaticQuery, graphql } from "gatsby";
 function IndexPage() {
   return (
-    <Layout>
-      <SEO
-        keywords={[
-          `Shopify Developer`,
-          `Freelancer`,
-          `Shopify App Developer`,
-          `Shopify Theme Developer`,
-          `Web App Developer`,
-          `Shopify Blog`,
-          `Developer Blog`,
-          `Developer Portfolio`,
-          `Web App Developer Portfolio`,
-          `Full Stack Developer Portfolio`,
-          `Shopify Developer Portfolio`,
-        ]}
-        title="Full Stack Developer, Shopify Developer, and Freelancer"
-      />
+    <StaticQuery
+      query={graphql`
+        {
+          heroImage: file(base: { eq: "setup.jpg" }) {
+            childImageSharp {
+              original {
+                src
+              }
+            }
+          }
+        }
+      `}
+      render={({ heroImage }) => (
+        <Layout>
+          <SEO
+            keywords={[
+              `Shopify Developer`,
+              `Freelancer`,
+              `Shopify App Developer`,
+              `Shopify Theme Developer`,
+              `Web App Developer`,
+              `Shopify Blog`,
+              `Developer Blog`,
+              `Developer Portfolio`,
+              `Web App Developer Portfolio`,
+              `Full Stack Developer Portfolio`,
+              `Shopify Developer Portfolio`,
+            ]}
+            title="Full Stack Developer, Shopify Developer, and Freelancer"
+            featuredImage={`https://iliashaddad.com${heroImage.childImageSharp.original.src}`}
+          />
 
-      <Hero />
-      <About />
-      <Skills />
-      <Work />
-      <Articles />
-      <Testimonials />
-    </Layout>
+          <Hero />
+          <About />
+          <Skills />
+          <Work />
+          <Articles />
+          <Testimonials />
+        </Layout>
+      )}
+    />
   );
 }
 
