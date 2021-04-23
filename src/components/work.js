eebce598ccc290405a60fdff9ea3b25a68fc1e7b
iliashad <iliasshaddad18@gmail.com>
2021-04-23 22:31:30 +0000

    Update porfolio section

--- src/components/work.js
+++ src/components/work.js
@@ -3,69 +3,69 @@ import { StaticQuery, graphql, Link } from "gatsby";
 import Img from "gatsby-image";
 export const Work = () => {
   return (
-    <section className="px-12 py-4 ">
-      <div className="w-2/4 ">
-        <h5 className="text-3xl font-bold">
-          What I&apos;ve been working on with my freelance clients.
-        </h5>
-      </div>
-      <div className="grid  grid-cols-1 md:grid-cols-2 py-12">
-        <p className="w-full text-lg">
-          I like to share what I worked on with my clients. Take a look at some
-          of the my work.
-        </p>
-      </div>
+    <StaticQuery
+      query={graphql`
+        query {
+          allGhostPage(
+            filter: { tags: { elemMatch: { slug: { eq: "client-project" } } } }
+          ) {
+            edges {
+              node {
+                title
+                custom_excerpt
+                slug
 
-      <div className="grid  grid-cols-1 md:grid-cols-2 py-12 gap-x-24 gap-y-12">
-        <StaticQuery
-          query={graphql`
-            query {
-              allGhostPage(
-                filter: {
-                  tags: { elemMatch: { slug: { eq: "client-project" } } }
-                }
-              ) {
-                edges {
-                  node {
-                    title
-                    custom_excerpt
-                    slug
-
-                    featureImageSharp {
-                      childImageSharp {
-                        fluid(maxWidth: 800, quality: 100, cropFocus: CENTER) {
-                          ...GatsbyImageSharpFluid
-                        }
-                      }
+                featureImageSharp {
+                  childImageSharp {
+                    fluid(maxWidth: 800, quality: 100, cropFocus: CENTER) {
+                      ...GatsbyImageSharpFluid
                     }
                   }
                 }
               }
             }
-          `}
-          render={({ allGhostPage }) => (
+          }
+        }
+      `}
+      render={({ allGhostPage }) => (
+        <section className="px-12 py-4 ">
+          {allGhostPage.edges.length > 0 && (
             <>
-              {allGhostPage.edges.map(({ node }, index) => (
-                <div key={index}>
-                  <Img
-                    imgStyle={{ objectFit: "cover" }}
-                    fluid={node.featureImageSharp.childImageSharp.fluid}
-                    className="rounded"
-                  />
+              <div className="w-2/4 ">
+                <h5 className="text-3xl font-bold">
+                  What I&apos;ve been working on with my freelance clients.
+                </h5>
+              </div>
+              <div className="grid  grid-cols-1 md:grid-cols-2 py-12">
+                <p className="w-full text-lg">
+                  I like to share what I worked on with my clients. Take a look
+                  at some of the my work.
+                </p>
+              </div>
 
-                  <p className="text-2xl font-semibold pt-4">{node.title}</p>
-                  <p className="text-lg text-gray-500 py-2">
-                    {node.custom_excerpt}
-                  </p>
-                  <button className=" pb-1 pt-2 text-lg border-b-2  border-indigo-100border-indigo-100">
-                    <Link to={`/project/${node.slug}`}>Read the story</Link>
-                  </button>
-                </div>
-              ))}
+              <div className="grid  grid-cols-1 md:grid-cols-2 py-12 gap-x-24 gap-y-12">
+                {allGhostPage.edges.map(({ node }, index) => (
+                  <div key={index}>
+                    <Img
+                      imgStyle={{ objectFit: "cover" }}
+                      fluid={node.featureImageSharp.childImageSharp.fluid}
+                      className="rounded"
+                    />
+
+                    <p className="text-2xl font-semibold pt-4">{node.title}</p>
+                    <p className="text-lg text-gray-500 py-2">
+                      {node.custom_excerpt}
+                    </p>
+                    <button className=" pb-1 pt-2 text-lg border-b-2  border-indigo-100border-indigo-100">
+                      <Link to={`/project/${node.slug}`}>Read the story</Link>
+                    </button>
+                  </div>
+                ))}
+              </div>
             </>
           )}
-        />
-      </div>
-    </section>
+        </section>
+      )}
+    />
   );
 };
