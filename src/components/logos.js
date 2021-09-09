bb2b9d5d206ff2d46efb4023989e6975dcc10b2e
iliashad <iliasshaddad18@gmail.com>
2021-09-09 21:33:27 +0100

    Add logos section

---
+++ src/components/logos.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+
+export const Logos = () => {
+  return (
+    <section classNameName="px-12 py-4">
+      <div>
+        <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+          <p className="text-center text-base font-semibold uppercase  tracking-wider">
+            Featured in
+          </p>
+          <div className="flex items-center justify-center md:flex-row flex-col">
+            <a
+              href="https://www.storetasker.com/experts/ilias-haddad"
+              className="flex justify-center md:py-4 px-8 md:w-1/5  w-8/12 mx-auto md:mx-0"
+              target="__blank"
+            >
+              <StaticImage
+                src="../images/storetasker.png"
+                placeholder="blurred"
+                quality={100}
+                layout="fullWidth"
+                className="w-full"
+
+              />
+            </a>
+            <a
+              href="https://www.builtinafrica.io/blog-post/ilias-haddad-morocco"
+              className="flex justify-center md:py-4 px-8 md:w-1/5  w-8/12 mx-auto  md:mx-0"
+              target="__blank"
+            >
+              <StaticImage
+                src="../images/builtinafrica.png"
+                placeholder="blurred"
+                quality={100}
+                layout="fullWidth"
+                className="w-full"
+
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
