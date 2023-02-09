a33b6b1fcac6827d13a9788b09be28f00a7e6700
Ilias Haddad <iliasshaddad18@gmail.com>
2023-02-09 11:09:51 +0100

    Fix fs module not found issue

--- next.config.js
+++ next.config.js
@@ -6,6 +6,16 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     domains: ['s3.us-west-2.amazonaws.com'],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.resolve.fallback = {
+        fs: false
+      }
+    }
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
