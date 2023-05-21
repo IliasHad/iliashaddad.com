f48364908fa58948bc0270e57d502cbae385aa1f
Ilias Haddad <iliasshaddad18@gmail.com>
2023-05-21 14:36:05 +0100

    update the website UI

--- pages/contact.js
+++ pages/contact.js
@@ -22,9 +22,11 @@ export default function Contact({ posts, featuredPost, sideProjects, clientProje
     const email = useRef();
     const message = useRef();
     const [success, setSuccess] = useState(false);
+    const [loading, setLoading] = useState(false);
     const plausible = usePlausible()
 
     const handleSubmit = (e) => {
+        setLoading(true)
         e.preventDefault();
         plausible("Project-Details-Sent");
         const data = {
@@ -36,6 +38,7 @@ export default function Contact({ posts, featuredPost, sideProjects, clientProje
         };
         axios.post(`/api/contact`, { data }).then((res) => {
             setSuccess(JSON.parse(res.data.success));
+            setLoading(false)
         });
     };
     return (
@@ -247,7 +250,13 @@ export default function Contact({ posts, featuredPost, sideProjects, clientProje
                                 justifySelf: "start",
                             }}
                         >
-                            <button className="flex mx-auto text-white bg-black border-0 py-2 px-8 focus:outline-none hover:bg-gray-600 rounded text-lg">
+                            <button className="flex items-center mx-auto text-white bg-gray-400 border-0 py-2 px-8 focus:outline-none hover:bg-gray-600 rounded text-lg">
+                                {loading &&
+                              <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                              <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                              <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                            </svg>
+                              }
                                 Submit
                             </button>
                         </div>
---
+++ pages/instagram.js
@@ -0,0 +1,110 @@
+import { Layout } from "../components/Layout";
+import { SEO } from "../components/Seo";
+import Image from "next/legacy/image";
+import Img from "../public/images/IMG_7434.jpg";
+import {
+  FaTwitter,
+  FaInstagram,
+  FaGithub,
+  FaLinkedin,
+  FaYoutube,
+} from "react-icons/fa";
+
+export default function Instagram({
+
+}) {
+  return (
+    <Layout>
+      <SEO
+        keywords={[
+          `Shopify Developer`,
+          `Freelancer`,
+          `Shopify App Developer`,
+          `Shopify Theme Developer`,
+          `Web App Developer`,
+          `Shopify Blog`,
+          `Developer Blog`,
+          `Developer Portfolio`,
+          `Web App Developer Portfolio`,
+          `Full Stack Developer Portfolio`,
+          `Shopify Developer Portfolio`,
+        ]}
+        title="Instagram"
+        description="@iliashaddad_dev Instagram"
+        featuredImage={"/images/IMG_7434.jpg"}
+        url={""}
+      />
+
+      <div class="mx-5">
+        <div class="">
+          <div class="max-w-lg h-auto mx-auto my-20 rounded-sm overflow-hidden shadow-lg bg-mirage-500">
+            <div className="h-32 mx-auto w-32">
+              <Image
+                src={Img}
+                alt={"Ilias Haddad"}
+                placeholder="blur"
+                objectFit="cover"
+                quality={100}
+                layout="responsive"
+                height={80}
+                width={80}
+                class="object-contain rounded-full mx-auto mt-8 p-1 border-4 border-yellow-500"
+              />
+            </div>
+            <div class="px-6 py-4 text-white">
+              <div class="flex flex-col">
+                <div class="font-bold text-xl text-center hover:cursor-pointer">
+                  @Iliashaddad_dev
+                </div>
+                <p class="text-sm text-center">Software Engineer</p>
+              </div>
+            </div>
+
+            <div class="flex flex-col mx-auto text-black text-center">
+              <a
+                href="https://twitter.com/KennKibadi"
+                target="__blank"
+                class="flex flex-row  justify-center mx-auto my-2 bg-white hover:bg-gray-100  w-2/4 text-mirage-500 py-2 pl-2 rounded-md border-r-4"
+              >
+                <FaTwitter className="h-6 w-6" />
+                <h4 class="my-auto  mx-5 text-black">Twitter</h4>
+              </a>
+              <a
+                href="https://www.youtube.com/channel/UCxv_k9zRHdz5L1av2xgEAMA"
+                target="__blank"
+                class="flex flex-row justify-center  mx-auto my-2 bg-white hover:bg-gray-100  w-2/4 text-mirage-500 py-2 pl-2 rounded-md border-r-4"
+              >
+              <FaYoutube className="h-6 w-6" />
+                <h4 class="my-auto  mx-5">Youtube</h4>
+              </a>
+              <a
+                href="https://github.com/iliashad"
+                target="__blank"
+                class="flex flex-row  justify-center mx-auto my-2 bg-white hover:bg-gray-100 w-2/4 text-mirage-500 py-2 pl-2 rounded-md border-r-4"
+              >
+              <FaGithub className="h-6 w-6" />
+                <h4 class="my-auto mx-5">Github</h4>
+              </a>
+              <a
+                href="https://www.instagram.com/iliashaddad_dev"
+                target="__blank"
+                class="flex flex-row  justify-center  mx-auto my-2 bg-white hover:bg-gray-100  w-2/4 text-mirage-500 py-2 pl-2 rounded-md border-r-4"
+              >
+              <FaInstagram className="h-6 w-6" />
+                <h4 class="my-auto  mx-3">Instagram</h4>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/ilias-haddad/"
+                target="__blank"
+                class="flex flex-row  justify-center  mx-auto my-2 bg-white hover:bg-gray-100  w-2/4 text-mirage-500 py-2 pl-2 rounded-md border-r-4"
+              >
+                <FaLinkedin className="h-6 w-6" />
+                <h4 class="my-auto mx-3">LinkedIn</h4>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
