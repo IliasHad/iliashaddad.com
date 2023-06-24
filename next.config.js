098d17daf1dd03464af7500d65c1473f387db417
Ilias Haddad <iliasshaddad18@gmail.com>
2023-06-24 22:46:16 +0100

    fix issue of timeout while upload big image file

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     domains: ['s3.us-west-2.amazonaws.com', 'ik.imagekit.io'],
   },
+  staticPageGenerationTimeout: 60 * 10,
   webpack: (config, { isServer }) => {
     if (!isServer) {
       // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
